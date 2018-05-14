6e27f6f0dc80b211e1df47a3ac4dad3d7acdcdff
root <root@ip-172-31-14-238.us-west-1.compute.internal>
2018-05-14 05:59:18 +0000

    terraform integration commit version 24

--- terraform_temlate1.tf
+++ terraform_temlate1.tf
@@ -1,6 +1,10 @@
-provider "aws" {
+variable "access_key" {}
+variable "secret_key" {}
 
-	region	= "us-east-1"
+provider "aws" {
+	access_key = "${var.access_key}"
+  	secret_key = "${var.secret_key}"
+  	region     = "us-east-1"
 }
 
 data "aws_ami" "node_app_ami" {
